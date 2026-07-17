8e53cdbc5e0d2308d248ebb85222f14060730881
hamzasand <mhamzasaeed762@gmail.com>
2026-07-17 16:38:41 +0500

    add the langserve base deployment

---
+++ main_langserve.py
@@ -0,0 +1,50 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from fastapi import FastAPI
+import uvicorn
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langserve import add_routes
+load_dotenv()
+
+GOOGLE_API_KEY=os.getenv("GOOGLE_API_KEY")
+LANGCHAIN_TRACING_V2=os.getenv("LANGCHAIN_TRACING_V2")
+LANGCHAIN_API_KEY=os.getenv("LANGCHAIN_API_KEY")
+
+# 1. Create prompt template
+system_template = "Translate the following into {language}:"
+
+prompt_template = ChatPromptTemplate.from_messages([
+    ('system', system_template),
+    ('user', '{text}')
+])
+
+# 2. Create model
+model = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite", temperature=0.7)
+
+# 3. Create parser
+parser = StrOutputParser()
+
+# 4. Create chain
+chain = prompt_template | model | parser
+
+app = FastAPI(
+    title="My LLM API",
+    description="My first LLM API",
+    version="1.0",
+)
+
+
+add_routes(
+    app,
+    chain,
+    path="/chain"
+    
+)
+
+
+if __name__=="__main__":
+    uvicorn.run(app, host="localhost", port=8000)
+
+    
