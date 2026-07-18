f301a04b9296d3b55c33298cefdea715662df2c7
hamzasand <mhamzasaeed762@gmail.com>
2026-07-18 20:08:25 +0500

    add embedding models

---
+++ embeddingmodels/embeddings.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+
+load_dotenv()
+
+embeddings= OpenAIEmbeddings(
+    model = 'text-embedding-3-large',
+    dimensions=64
+)
+
+texts = [
+    "Hello this is Akarsh Vyas",
+    "Hello your name is YouTube",
+    "And you all are very beautiful"
+]
+
+vector = embeddings.embed_documents(texts)
+print(vector)
