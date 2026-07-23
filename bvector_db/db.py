ce049cc1f21f29613360e98275b9463e39bf2045
hamzasand <mhamzasaeed762@gmail.com>
2026-07-23 20:43:45 +0500

    code update

--- bvector_db/db.py
+++ bvector_db/db.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
-from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import Chroma
 from langchain_core.documents import Document
 
 load_dotenv()
