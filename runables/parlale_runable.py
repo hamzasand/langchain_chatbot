e69eae855c4a3eb1fb410362dc64c38d5f5d3335
hamzasand <mhamzasaeed762@gmail.com>
2026-07-31 20:53:26 +0500

    update parallelrunnables

--- runables/parlale_runable.py
+++ runables/parlale_runable.py
@@ -3,8 +3,8 @@ from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableParallel, RunnableLambda
-
 load_dotenv()
+
 # compoenents
 model = ChatMistralAI(model="mistral-small-2506")
 parser = StrOutputParser()
