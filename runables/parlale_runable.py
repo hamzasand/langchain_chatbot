8746ad032661b4d8713cc35a72c35d46ea2df46b
hamzasand <mhamzasaeed762@gmail.com>
2026-07-31 17:40:07 +0500

    add parallelrunables, lambdarunnables

---
+++ runables/parlale_runable.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel, RunnableLambda
+
+load_dotenv()
+# compoenents
+model = ChatMistralAI(model="mistral-small-2506")
+parser = StrOutputParser()
+
+# set two different prompt for parallelrunnables
+short_prompt = ChatPromptTemplate.from_template(
+    "Explain {topic} under 2 lines."
+)
+detailed_prompt = ChatPromptTemplate.from_template(
+    "Explain {topic} in details."
+) 
+
+chain = RunnableParallel({
+    "short": RunnableLambda(lambda x :x['short']) |short_prompt| model| parser,
+    "detailed": RunnableLambda(lambda x : x['detailed']| detailed_prompt| model| parser)
+})
+
+result = chain.invoke({
+    "short":{"topic":"Machine Leaning"},
+    "detailed":{"topic":"Deep Learning"}
+})
+
+print(result['short'])
+print(result['detailed'])
