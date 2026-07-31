a73fca42fc7adab84127e84144901fc691c3b106
hamzasand <mhamzasaeed762@gmail.com>
2026-07-31 20:31:57 +0500

    update runnable passthrough add

---
+++ runables/runable_passthrough.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+from langchain_mistralai import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+
+load_dotenv()
+
+model = ChatMistralAI(model="mistral-small-2506")
+parser = StrOutputParser()
+
+code_prompt = ChatPromptTemplate.from_messages([
+    ("system","You are a code generator in python"),
+    ("human","{topic}")
+])
+
+explain_code= ChatPromptTemplate.from_messages([
+    ("system","You are helpful assistnt who explain code in simple terms"),
+    ("human","Explain this code in simple words:\n{code}")
+])
+
+seq = code_prompt | model | parser
+seq2 = RunnableParallel(
+    {"code": RunnablePassthrough(),
+     "explanation": explain_code | model | parser}
+)
+
+chain = seq | seq2
+result = chain.invoke({"topic": "Please write a code of palindrome in python"})
+print(result['code'])
+print(result['explanation'])
